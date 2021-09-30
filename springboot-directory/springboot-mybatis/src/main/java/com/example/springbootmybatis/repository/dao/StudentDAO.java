9ac8c8f6c315dc697e6f6492e658a2aee6820525
huchangliang <157223522@qq.com>
2021-09-30 15:04:29 +0800

    feat: springboot-mybatis xml方式整合

---
+++ springboot-directory/springboot-mybatis/src/main/java/com/example/springbootmybatis/repository/dao/StudentDAO.java
@@ -0,0 +1,16 @@
+package com.example.springbootmybatis.repository.dao;
+
+import lombok.Data;
+
+/**
+ * @author Hu.ChangLiang
+ * @date 2021/9/30 2:16 下午
+ */
+@Data
+public class StudentDAO {
+    private Long id;
+
+    private String name;
+
+    private Integer age;
+}
---
+++ springboot-directory/springboot-mybatis/src/main/java/com/example/springbootmybatis/repository/mapper/StudentMapper.java
@@ -0,0 +1,21 @@
+package com.example.springbootmybatis.repository.mapper;
+
+import com.example.springbootmybatis.repository.dao.StudentDAO;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author Hu.ChangLiang
+ * @date 2021/9/30 2:12 下午
+ */
+
+public interface StudentMapper {
+
+    StudentDAO findById(@Param("id") Long id);
+
+    List<StudentDAO> findByParam(@Param("params") Map<String, Object> params);
+
+    List<StudentDAO> findByStudent(@Param("student") StudentDAO studentDAO);
+}
