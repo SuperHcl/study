38a00c5922497aff70a0f63234a6511651664368
huchangliang <157223522@qq.com>
2021-12-08 17:22:29 +0800

    test

--- spring-cloud/00-eurekaserver-8001/src/main/java/com/hcl/example/Application.java
+++ spring-cloud/00-eurekaserver-8001/src/main/java/com/hcl/example/Application.java
@@ -5,7 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaServer     // 开启eureka服务
+@EnableEurekaServer
+// 开启eureka服务
 public class Application {
 
     public static void main(String[] args) {
