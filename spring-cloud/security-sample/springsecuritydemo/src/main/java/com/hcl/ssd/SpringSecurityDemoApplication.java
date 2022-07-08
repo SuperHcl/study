20373d213661df398752b8dd5bb621e9147398ea
huchangliang <157223522@qq.com>
2022-07-08 11:14:28 +0800

    feat: @PreAuthorize注解的作用和access表达式的功能是一样的, 同样需要在启动类中开始prePostEnabled=true的属性

--- spring-cloud/security-sample/springsecuritydemo/src/main/java/com/hcl/ssd/SpringSecurityDemoApplication.java
+++ spring-cloud/security-sample/springsecuritydemo/src/main/java/com/hcl/ssd/SpringSecurityDemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 @SpringBootApplication
-@EnableGlobalMethodSecurity(securedEnabled = true)
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
 public class SpringSecurityDemoApplication {
 
     public static void main(String[] args) {
--- spring-cloud/security-sample/springsecuritydemo/src/main/java/com/hcl/ssd/controller/LoginController.java
+++ spring-cloud/security-sample/springsecuritydemo/src/main/java/com/hcl/ssd/controller/LoginController.java
@@ -2,6 +2,7 @@ package com.hcl.ssd.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,7 +17,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Slf4j
 public class LoginController {
 
-    @Secured("ROLE_abc2")
+//    @Secured("ROLE_abc2")
+    // PreAuthorize注解的功能就是配置类中的access
+    // 有个比配置类强大的功能点是，hasRole可以ROLE_开头，也可以不用ROLE_开头，而配置中只能以ROLE_开头
+    @PreAuthorize("hasRole('abc')")
     @RequestMapping("toMain")
     public String toMain() {
         log.info("...跳转登录页面....");
